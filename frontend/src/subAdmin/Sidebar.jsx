e779e4fba84d3b9af3353aae9ee25906f80d8bf5
NextGenProductLabs <nextgenproductlabs@gmail.com>
2025-11-14 20:59:47 +0530

    top bar added

--- frontend/src/subAdmin/Sidebar.jsx
+++ frontend/src/subAdmin/Sidebar.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import Logo from '../home/components/Logo'
+import Logo from '../home/components/Logo';
+
 
 function Sidebar({toggle}) {
   return (
     <aside className="w-64 bg-white border-r h-screen sticky top-0 p-4">
-        
-        <div className='flex items-center justify-between mb-8 space-x-2 '>
-            
-            <Logo />
-            <div onClick={()=>{
-              toggle(false)}}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48"><defs><mask id="SVGtzNlLcMt"><g fill="none" stroke-linejoin="round" stroke-width="4"><path fill="#fff" stroke="#fff" d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"/><path stroke="#000" stroke-linecap="round" d="m27 33l-9-9l9-9"/></g></mask></defs><path fill="#a6a6a6" d="M0 0h48v48H0z" mask="url(#SVGtzNlLcMt)"/></svg>
-            </div>
-         </div>
+      <div className="flex items-center justify-between mb-8">
+        <Logo/>
+        <div className='pl-4' onClick={()=>{
+          toggle(true)
+        }}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48"><defs><mask id="SVGtzNlLcMt"><g fill="none" stroke-linejoin="round" stroke-width="4"><path fill="#fff" stroke="#fff" d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"/><path stroke="#000" stroke-linecap="round" d="m27 33l-9-9l9-9"/></g></mask></defs><path fill="#a6a6a6" d="M0 0h48v48H0z" mask="url(#SVGtzNlLcMt)"/></svg>
+        </div>
+      </div>
 
       <nav className="space-y-3">
         <NavLink
--- frontend/src/subAdmin/pages/Dashboard.jsx
+++ frontend/src/subAdmin/pages/Dashboard.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Dashboard() {
   return (
-    <h1 className="text-2xl font-bold">Dashboard Content Here</h1>
+    <div>
+       <h1 className="text-2xl font-bold">Dashboard</h1>
+    </div>
   )
+    
+   
 }
 
 export default Dashboard
